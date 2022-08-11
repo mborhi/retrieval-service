ffad9bfa0d05e10bb007fed6e345102061254c2f
Marcell Borhi <mborhi@iu.edu>
2022-08-11 17:26:51 -0400

    add server routes

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,10 +1,23 @@
 import express from 'express';
-import { getCategoryPlaylist } from '../utils/categories-utils';
+import { loadCategories } from '../utils/categories/fetch-categories';
+import { getCategoryPlaylist } from '../utils/categories/fetch-category-id';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('hello from categories route');
+router.get('/', async (req, res) => {
+    const token = req.headers.access_token as string;
+    try {
+        const categories = await loadCategories(token,);
+        res.status(200).json({ "data": categories });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            "error": {
+                "status": 500,
+                "message": "internal server error"
+            }
+        });
+    }
 });
 
 router.get('/:category_id', async (req, res) => {
--- src/routes/genres.ts
+++ src/routes/genres.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { getGenreTracks } from '../utils/genres/fetch-genre-id';
 
 const router = express.Router();
 
@@ -7,8 +8,22 @@ router.get('/', (req, res) => {
     res.send('hello from genres endpoint');
 });
 
-router.get('/:genre_id', (req, res) => {
-    res.send('hello from the genre id endpoint');
+router.get('/:genre_id', async (req, res) => {
+    const access_token = req.headers.access_token as string;
+    const genre_id = req.params.genre_id;
+
+    try {
+        const genreTracks = await getGenreTracks(access_token, genre_id);
+        res.status(200).json({ "data": genreTracks });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            "error": {
+                "status": 500,
+                "message": "internal server error"
+            }
+        });
+    }
 });
 
 export default router;
