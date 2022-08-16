e090d69bc898774cb46ba2387345b46488484cf6
Marcell Borhi <mborhi@iu.edu>
2022-08-16 13:16:22 -0400

    add utility functions for retrieving users playlist tracks and creating new playlist

--- src/utils/playlist/modify-playlist.ts
+++ src/utils/playlist/modify-playlist.ts
@@ -1,4 +1,53 @@
-export const getUserPlaylistTracks = async (token: string, user_id: string) => {
+import endpointsConfig from "../../../endpoints.config";
+import fetch from "node-fetch";
+import { stringify } from 'querystring';
+import { responseIsError } from '../fetch-utils';
+import { getPlayListTracks } from "./playlist-data";
+
+const baseURL = endpointsConfig.SpotifyAPIBaseURL;
+
+/**
+ * Retrieves the given user's paylists
+ * Uses Get User's Playlists Spotify Web API call:
+ * 
+ * API Reference	https://developer.spotify.com/documentation/web-api/reference/#/operations/get-list-users-playlists
+ * 
+ * Endpoint	        https://api.spotify.com/v1/users/{user_id}/playlists
+ * 
+ * HTTP Method	    GET
+ * 
+ * OAuth	        Required
+ * @param token the users OAuth2 access token
+ * @param user_id the id of the user
+ * @param limit the maximum number of playlists to recieve
+ * @param offset the offset of playlists
+ */
+export const getUserPlaylistTracks = async (token: string, user_id: string, limit = 50, offset = 0) => {
+    const queryParams = {
+        limit: limit,
+        offset: offset
+    }
+    const url = baseURL + `users/${user_id}/playlists?` + stringify(queryParams);
+    const response = await fetch(url, {
+        method: "GET",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
+    if (responseIsError(response)) return await response.json();
+    try {
+        const data: SpotifyApi.ListOfUsersPlaylistsResponse = await response.json();
+        // find the playlist
+        const playlists = data.items;
+        const quickDiscoverPlaylist = playlists.find((playlist) => playlist.name === endpointsConfig.QuickDiscoverPlaylistName) || await createNewPlaylist(token, user_id);
+        // get the tracks
+        const tracks = await getPlayListTracks(token, quickDiscoverPlaylist);
+        return tracks; // SpotifyError will be handled by caller
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
 
 };
 
@@ -13,4 +62,43 @@ export const addToPlaylist = async (token: string) => {
 export const removeFromPlaylist = async (token: string) => {
 
 };
+
+/**
+ * Creates a new private playlist with the name and description specified in the endpoints config
+ * Uses Create Playlist Spotify Web API call:
+ * API Reference	https://developer.spotify.com/documentation/web-api/reference/#/operations/create-playlist
+ * 
+ * Endpoint	        https://api.spotify.com/v1/users/{user_id}/playlists
+ * 
+ * HTTP Method	    POST
+ * 
+ * OAuth	        Required
+ * @param token the users OAuth2 access token
+ * @param user_id the id of the user to create playlist for
+ * @returns the newly created playlist
+ */
+export const createNewPlaylist = async (token: string, user_id: string): Promise<SpotifyApi.PlaylistObjectSimplified> => {
+    const bodyParams = {
+        "name": endpointsConfig.QuickDiscoverPlaylistName,
+        "description": "Songs discovered through the Quick Discover web app",
+        "public": false
+    }
+    const url = baseURL + `/users/${user_id}/playlists`;
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        },
+        body: JSON.stringify(bodyParams)
+    });
+    if (responseIsError(response)) return await response.json();
+    try {
+        const data: SpotifyApi.CreatePlaylistResponse = await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
 
---
+++ src/utils/playlist/playlist-data.ts
@@ -0,0 +1,69 @@
+import fetch from 'node-fetch';
+import { stringify } from 'querystring';
+import { TrackData } from '../../../interfaces/index';
+import { responseIsError } from '../fetch-utils';
+import { addToPlaylist } from './modify-playlist';
+
+const baseURL = process.env.SPOTIFY_BASE_URL;
+
+/**
+ * Gets the TrackData for every track in the given playlist's tracks.
+ * Uses Get Playlist Spotify Web API call:
+ * 
+ * API Reference	https://developer.spotify.com/documentation/web-api/reference/#/operations/get-playlist
+ * 
+ * Endpoint	        https://api.spotify.com/v1/playlists/{playlist_id}
+ * 
+ * HTTP Method	    GET
+ * 
+ * OAuth	        Required
+ * @param {string} token                the user's OAuth2 token
+ * @param {SpotifyApi.PlaylistObjectSimplified} playlist   the playlist to get the tracks of
+ * @param {string} [fields='tracks']    the type to return
+ * @param {string} [market='US']        the market to return tracks from
+ * @returns {TrackData[]}               an array of the playlists' tracks data (name, previewURL)
+ * @throws Will throw an error if playlists and tracks cannot be retrieved correctly.
+ */
+export const getPlayListTracks = async (token: string, playlist: SpotifyApi.PlaylistObjectSimplified, fields = 'tracks', market = 'US'): Promise<TrackData[] | SpotifyApi.ErrorObject> => {
+    const query = {
+        fields: fields,
+        market: market
+    }
+    const url = baseURL + '/playlists/' + playlist.id + '?' + stringify(query);
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    });
+    if (responseIsError(response)) return await response.json();
+    try {
+        const data: SpotifyApi.SinglePlaylistResponse = await response.json();
+        const playlistTracks = data.tracks.items; // list of spotify_tracks
+        // for every track in tracks, get the name of the track and the preview url
+        const listOfPlaylistTracks = playlistTracks.map((playlistTrack) => {
+            // add a function to handle filtering of nulls in this
+            const trackData = {
+                name: playlistTrack.track.name,
+                previewURL: playlistTrack.track.preview_url,
+                trackURI: playlistTrack.track.uri,
+                trackNum: playlistTrack.track.track_number,
+                trackAlbumImage: playlistTrack.track.album.images[0].url
+            };
+            return trackData;
+        });
+        return listOfPlaylistTracks;
+    } catch (error) {
+        console.error("Error: ", error);
+        throw error;
+    }
+}
+
+export const findPlaylist = (playlists: SpotifyApi.PlaylistObjectSimplified[], name: string, token: string) => {
+    let playlist = playlists.find((playlist) => playlist.name === name);
+    return playlist.id ?? null;
+    // addNewPlaylist(token)
+
+}
